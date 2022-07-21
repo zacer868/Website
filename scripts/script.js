94e2058beecd8a0366fbac9a84545525a8ee99ce
Giovanni <zacergames.tt@gmail.com>
2022-07-20 21:15:23 -0400

    git commit -a

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,42 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times'); 
-    navbar.classList.remove('active');
-}
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
 
-var swiper = new Swiper(".home-slider", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2,
-      slideShadows: true,
-    },
-    // pagination: {
-    //   el: ".swiper-pagination",
-    loop:true,
-    autoplay:{
-        delay: 1000,
-        disableOnInteraction:false,
-    },
-  });
+// var swiper = new Swiper(".home-slider", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//       rotate: 0,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 2,
+//       slideShadows: true,
+//     },
+//     // pagination: {
+//     //   el: ".swiper-pagination",
+//     loop:true,
+//     autoplay:{
+//         delay: 1000,
+//         disableOnInteraction:false,
+//     },
+//   });
 
-  function openCity(cityName) {
-    var i;
-    var x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    document.getElementById(cityName).style.display = "block";  
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  document.getElementById(cityName).style.display = "block";
+}
